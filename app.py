888abdefa670fb8fe1c171e848dc54eb534234f1
Mark <mark@example.dev>
2021-08-18 05:31:30 -0700

    user registration

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 """User Auth Flask App"""
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, flash
 from models import db, connect_db, User
 from forms import RegisterForm
+from sqlalchemy import exc
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'YOUR_KEY_HERE'
@@ -10,12 +11,35 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
 connect_db(app)
+db.create_all()
 
 @app.route('/')
 def home_view():
     return redirect('/register')
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register_view():
     form = RegisterForm()
-    return render_template('register.html', form=form)
+
+    if form.validate_on_submit():
+        new_user = User.register(
+            username = form.username.data, password = form.password.data,
+            email = form.email.data, first_name = form.first_name.data,
+            last_name = form.last_name.data
+        )
+
+        db.session.add(new_user)
+        try:
+            db.session.commit()
+            return redirect('/secret')
+        except exc.IntegrityError:
+            db.session.rollback()
+            flash('An account is already registered with that username/email.')
+            return redirect('/register')
+
+    else:
+        return render_template('register.html', form=form)
+
+@app.route('/secret')
+def secret_view():
+    return "Welcome to the secret"
--- models.py
+++ models.py
@@ -1,8 +1,10 @@
 """SQLAlchemy Models for User Auth Flask App"""
 
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 db = SQLAlchemy()
+bcrypt = Bcrypt()
 
 def connect_db(flask_app):
     db.app = flask_app
@@ -10,9 +12,22 @@ def connect_db(flask_app):
 
 class User(db.Model):
 
+    __tablename__ = 'users'
+
     username = db.Column(db.String(20), primary_key=True)
     password = db.Column(db.String, nullable=False)
     email = db.Column(db.String(50), nullable=False, unique=True)
     first_name = db.Column(db.String(30), nullable=False)
     last_name = db.Column(db.String(30), nullable=False)
 
+    @classmethod
+    def register(cls, username, password, email, first_name, last_name):
+        """Generates a hash for the given plaintext password and returns a model instance with hashed passwrord"""
+        pw_hash = bcrypt.generate_password_hash(password)
+        hash_utf8 = pw_hash.decode('utf8')
+
+        return cls(username=username, password=hash_utf8, email=email, first_name=first_name, last_name=last_name)
+
+    @classmethod
+    def authenticate(cls, username, password):
+        pass
